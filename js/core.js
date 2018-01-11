67c358b6ef491a47c4c30babd29a5a2bcfea8e6e
Ørjan Polden <op@mlabs.no>
2018-01-11 10:09:43 +0100

    Finish menu list, add animated collapse to menu groups

--- js/core.js
+++ js/core.js
@@ -11,5 +11,22 @@
             $('#sidebar').removeClass('open');
         });
 
+        /**
+         * Setup toggle on menu groups
+         */
+        $('.menu-group-title').on('click', function () {
+            $next = $(this).next('.menu-group');
+
+            if ($next.css('display') == 'none') {
+                $next.show('400');
+                $(this).addClass('open');
+            }
+            else {
+                $next.hide('400');
+                $(this).removeClass('open');
+            }
+
+        })
+
     });
 })(jQuery);
